225a6f6cd9c63c87932b1db6024726ce65ffd7a4
Mveneziale <melyveneziale@gmail.com>
2022-10-27 20:52:07 -0300

    17 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -105,12 +105,12 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( num % 2 ===0 ){
+  if ( num % 2 === 0 ){
     return true;
   } else {
     return false;
   }
-}
+} 
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
