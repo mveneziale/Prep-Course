9e7e3eacdd65623c97d5440e1ed542f75203564e
Mveneziale <melyveneziale@gmail.com>
2022-10-27 20:05:10 -0300

    segundo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 37;
+const nuevoNum = 36;
 
 // Crea una variable booleana:
 const nuevoBool = true;
