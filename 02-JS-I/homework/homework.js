6172832fcc154744b30d0d4792eac0b3ab5795f9
Mveneziale <melyveneziale@gmail.com>
2022-10-27 20:26:19 -0300

    noveno commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -42,7 +42,7 @@ function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   return x - y;
-}
+} 
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
