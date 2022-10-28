d37d05a3270897d25c30fc60fbe05c0f0503bd1b
Mveneziale <melyveneziale@gmail.com>
2022-10-27 21:00:01 -0300

    19 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -127,8 +127,8 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
-}
+  return Math.pow(num, 2);
+} 
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
