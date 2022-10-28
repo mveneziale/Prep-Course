4cf2f99b877988f5d15d90247b64b96ad4d95bc1
Mveneziale <melyveneziale@gmail.com>
2022-10-27 22:30:43 -0300

    32 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -237,8 +237,16 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
-}
+  if(letra.lenght > 1){
+    return "Dato Incorrecto";
+  }
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+    return "Es Vocal"
+  }
+  return "Dato Incorrecto";
+} 
+
+
 
 
 
