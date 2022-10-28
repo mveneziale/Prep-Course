faec8f57b99d961d60b7da752d3642b0a2117268
Mveneziale <melyveneziale@gmail.com>
2022-10-27 22:19:40 -0300

    29 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,8 +212,8 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
-}
+  lado * 4;
+} 
 
 
 function areaDelTriangulo(base, altura){
