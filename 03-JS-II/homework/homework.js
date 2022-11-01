0744bee0a13eef845ad6bb1843c0e1b201a9ffcb
Mveneziale <melyveneziale@gmail.com>
2022-10-31 21:55:37 -0300

    commit js 2 9

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -108,7 +108,11 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-}
+  if(numero % 15 === 0) return 'fizzbuzz';
+  if(numero % 3 === 0) return 'fizz';
+  if(numero % 5 === 0) return 'buzz';
+  return numero;
+} 
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
