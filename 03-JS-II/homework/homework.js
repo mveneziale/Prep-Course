a49fc2e1fc365bde0bdae807580f86ecebc87e4c
Mveneziale <melyveneziale@gmail.com>
2022-10-31 21:44:24 -0300

    commit js 2 6

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -82,7 +82,8 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-}
+  return numero === 10 || numero === 5;
+} 
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
