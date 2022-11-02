2e552d61d192ecad02ff5b33fb2fc764f975f4e6
Mveneziale <melyveneziale@gmail.com>
2022-11-01 21:10:20 -0300

    commit js 2 12

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -155,12 +155,17 @@ function esPrimo(numero) {
 } 
 
 
+
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor === true){
+    return "Soy verdadero"
+  }
+  return "Soy falso"
+} 
 
-}
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
