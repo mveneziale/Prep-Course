a5c11a40521df55cd908f31b21184710b696d969
Mveneziale <melyveneziale@gmail.com>
2022-11-01 21:25:46 -0300

    commit js 2 14

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,8 +181,12 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
-}
+  if(numero >= 100 && numero < 1000){
+    return "true";
+  } else{
+    return "false";
+  }
+} 
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
