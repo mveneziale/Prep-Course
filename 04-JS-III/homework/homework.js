236959966a0cffa5489f70faf7ee400b6ad11dfd
Mveneziale <melyveneziale@gmail.com>
2022-11-07 20:26:32 -0300

    commit js 3 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,7 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-}
+var nuevoArray = [];
+for(var i = 0; i < array.lenght; i++){
+  nuevoArray[i] + 1;
+} return nuevoArray;
+} 
 
 
 function agregarItemAlFinalDelArray(array, elemento) {
