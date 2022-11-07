cee9ffd1929c206b51f8bfe1188992795f6b880c
Mveneziale <melyveneziale@gmail.com>
2022-11-07 20:59:18 -0300

    commit js 3 12

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -99,7 +99,14 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-}
+  var maximo = numeros[0];
+  for(var i = 1; i < numeros.lenght; i++){
+    if(numeros[i] > maximo){
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
+} 
 
 
 function multiplicarArgumentos() {
