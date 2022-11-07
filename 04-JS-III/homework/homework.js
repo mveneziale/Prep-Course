e7cee9befbd03c0705f373e500208e804816b142
Mveneziale <melyveneziale@gmail.com>
2022-11-07 20:40:54 -0300

    commit js 3 8

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -58,8 +58,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-
-}
+  return palabras.join(" ");
+} 
 
 
 function arrayContiene(array, elemento) {
