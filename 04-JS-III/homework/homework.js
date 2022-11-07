68fbaafb97a1d1d4bcaca3bcfbbed4f15702d064
Mveneziale <melyveneziale@gmail.com>
2022-11-07 20:33:05 -0300

    commit js 3 6

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -37,7 +37,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-}
+  array[array.push] = elemento;
+  return array;
+} 
 
 
 function agregarItemAlComienzoDelArray(array, elemento) {
