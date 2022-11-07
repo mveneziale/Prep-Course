d7c2710b3149355a925278b9f3df1300c5ac5a5d
Mveneziale <melyveneziale@gmail.com>
2022-11-07 20:14:42 -0300

    commit js 3 1

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,7 +3,8 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
-}
+  return array[0];
+} 
 
 
 function devolverUltimoElemento(array) {
