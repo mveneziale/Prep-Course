31f7e512f82b0a47405a7e73daf725d731cbffa6
Mveneziale <melyveneziale@gmail.com>
2022-11-07 21:33:45 -0300

    commit js 3 16

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -163,7 +163,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+ for(var i = 0; i < arreglo.lenght -1; i++){
+  if(arreglo[i] !== arreglo[i+1]){
+    return false
+  }
+ } 
+ return true
 } 
 
 
