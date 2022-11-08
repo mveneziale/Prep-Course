6bbbea219354f020bfa8e1afe5b71fbd0445cbd9
Mveneziale <melyveneziale@gmail.com>
2022-11-07 21:29:33 -0300

    commit js 3 15

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -151,8 +151,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
-}
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true 
+  }
+  return false
+} 
 
 
 function todosIguales(arreglo) {
